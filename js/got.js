176e6491f1e9de0151188120ee46f902268264b6
Andras Terjek <andras.terjek@gmail.com>
2018-12-10 00:36:01 +0100

    add click & search feature & mod required css

--- js/got.js
+++ js/got.js
@@ -9,8 +9,11 @@ function getGameOfThronesCharacterDatas(url, callbackFunc) {
   xhttp.send();
 }
 
+// Live servert használd mindig!!!!!
+/* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+
 function getJustAlive(data) {
-  console.log(Array.isArray(data));
+  // console.log(Array.isArray(data));
   var justAlive = [];
   for (var k = 0; k < data.length; k += 1) {
     if (data[k].dead !== true) {
@@ -30,49 +33,119 @@ function sortCharacters(data) {
     return -1;
   });
 }
+// Ez egy példa arra, hogyan tudok egy loop segítségével eventListenert
+// hozzáadni több elemhez
+// Ebben a példávan a nodokhoz kapcsolódó függvényeket használtam
 
-function displayNamesStatic(data) {
-  var charExtra = document.querySelector('.portraits');
-  // console.log(charExtra);
-  for (var k = 0; k < data.length; k += 1) {
-    charExtra.innerHTML +=
-      `
-    <article class="charDetailsPerson">
-      <img class="charPicture${k}" src="${data[k].portrait}" alt="${data[k].name}">
-      <p class="charName${k}"> ${data[k].name} </p>
-    </article>  
-    `;
-    clickPerson();
-  }
+// Csak egy function a container element lekérésére
+function getCharPortraitsElement() {
+  var charPortraits = document.querySelector('.portraits');
+  return charPortraits;
 }
 
-function clickPerson() {
-  var charClassName = document.querySelector('.charDetailsPerson');
-  charClassName.addEventListener('click', function clickPers() {
-    console.log(charClassName);
-    getCharDetails();
+// Ez a függvény megjeleníti annak a p tagnek a tartalmát alerten, amire kattintottunk.
+function showElementContent(index, characters) {
+  var charPortraits = getCharPortraitsElement();
+  var paragraph = charPortraits.children;
+  // alert(paragraph[index].textContent);
+  clickfilterCharacter(characters, index);
+}
+
+function addCustomListenerForParagraph(element, index, characters) {
+  element.addEventListener('click', function clickCharacter() {
+    showElementContent(index, characters);
   });
 }
 
-function getCharDetails(data) {
-  var k = 0;
-  console.log(data.length);
-  while (k < data[k].length) {
-    if (data[k].hasOwnProperty) {
-      // if (gameOfThronesCharacters[i].organization) {
-      document.querySelector('.charDetailsTbody').innerHTML +=
-        `
-        <article>
-        <img class ="charPicture"
-        src="${data[k].picture}"
-        alt="${data[k].name}">
-        <p class="charName">${data[k].name}</p>
-        <p class="charBio">${data[k].bio}</p>
-        </article>
-        `;
-    }
-    k++;
+function generateElements(data, characters) {
+  var charPortraits = getCharPortraitsElement();
+  for (var k = 0; k < data.length; k += 1) {
+    var paragraph = document.createElement('p');
+    paragraph.innerHTML +=
+      `
+    <article class="charDetailsPerson">
+      <img class="charPicture" src="${data[k].portrait}" alt="${data[k].name}">
+      <p class="charName"> ${data[k].name} </p>
+    </article>  
+    `;
+    // paragraph.textContent = displayNamesStatic(data);
+    addCustomListenerForParagraph(paragraph, k, characters);
+    charPortraits.appendChild(paragraph);
   }
+}
+
+// add search
+
+function gotSearch(characters) {
+  document.querySelector('.charDetailsInputButton').addEventListener('click', function clickGoTChars() {
+    filterCharacter(characters);
+  });
+}
+
+function filterCharacter(characters) {
+  var charDetailsInfo = document.querySelector('.charDetailsInfo');
+  var charDetailsInfoHTML = '';
+  var charSearch = document.querySelector('.charDetailsInputText').value;
+  var charName = document.querySelector('.charName').value;
+  console.log(name);
+  console.log(charSearch);
+  // console.log(characters.length);
+  for (var i = 0; i < characters.length; i++) {
+    var charHouse = '';
+    console.log(characters[i].name);
+    if (characters[i].house) {
+      charHouse = `<img src="./assets/houses/${characters[i].house}.png" alt="${characters[i].house}"></img>`;
+    } else {
+      charHouse = '';
+    }
+    charDetailsInfoHTML =
+      `
+    <img class="charDetailsImg" src="${characters[i].picture}"></img>
+    <p class="charDetailsName">${characters[i].name}</p>
+    <p class="charDetailsHouse">${charHouse}</p>
+    <p class="charDetailsBio">${characters[i].bio}</p>
+    `;
+    if (characters[i].name.toLowerCase() === charSearch.toLowerCase()) {
+      charDetailsInfo.innerHTML = charDetailsInfoHTML;
+      // console.log(i);
+      break;
+    // } else if (characters[i].name.toLowerCase() === charName.toLowerCase()) {
+    //   charDetailsInfo.innerHTML = charDetailsInfoHTML;
+    //   console.log(charName);
+    //   break;
+    } else {
+      charDetailsInfo.innerHTML = 'Character not found';
+    }
+  }
+}
+
+function clickfilterCharacter(characters, index) {
+  for (var i = 0; i < characters.length; i++) {
+    var charHouse = '';
+    var charDetailsInfoHTML = '';
+    var charDetailsInfo = document.querySelector('.charDetailsInfo');
+    if (characters[i].house) {
+      charHouse = `<img src="./assets/houses/${characters[i].house}.png" alt="${characters[i].house}"></img>`;
+    } else {
+      charHouse = '';
+    }
+    charDetailsInfoHTML =
+      `
+    <img class="charDetailsImg" src="${characters[index].picture}"></img>
+    <p class="charDetailsName">${characters[index].name}</p>
+    <p class="charDetailsHouse">${charHouse}</p>
+    <p class="charDetailsBio">${characters[index].bio}</p>
+    `;
+    charDetailsInfo.innerHTML = charDetailsInfoHTML;
+    // charDetailsInfo.innerHTML = 'Character not found';
+  }
+}
+
+// add search to p
+function gotSearchName(characters) {
+  document.querySelector('.charDetailsInputButton').addEventListener('click', function clickChar() {
+    filterCharacter(characters);
+  });
 }
 
 function successGetGameOfThronesCharacterDatas(xhttp) {
@@ -80,10 +153,12 @@ function successGetGameOfThronesCharacterDatas(xhttp) {
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen hívhatod meg a többi függvényed
   var livingCharacters = getJustAlive(userDatas);
-  console.log(livingCharacters);
+  // console.log(livingCharacters);
   sortCharacters(livingCharacters);
-  displayNamesStatic(livingCharacters);
-  // displayNamesStatic(sortedCharacters);
+  // displayNamesStatic( livingCharacters);
+  gotSearch(livingCharacters);
+  gotSearchName(livingCharacters);
+  generateElements(livingCharacters, livingCharacters);
 }
 
 
@@ -92,5 +167,4 @@ getGameOfThronesCharacterDatas(
   successGetGameOfThronesCharacterDatas
 );
 
-// Live servert használd mindig!!!!!
-/* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+
